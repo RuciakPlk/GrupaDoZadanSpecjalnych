4f557c1a1a594979cf81df40e20748154baabc77
RuciakPlk <wojrucinski@gmail.com>
2023-02-05 15:39:21 +0100

    Wszystko od Mateusza

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.Multithreading.DeadlockExample;
+import org.example.Multithreading.SynchronizedCounter;
+
+public class App {
+    public static void main( String[] args ) throws InterruptedException {
+        DeadlockExample.example();
+    }
+}
